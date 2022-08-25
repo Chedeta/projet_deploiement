38eaea62ec41e5b53d9bcbaa995c187a034f2c4b
Chedeta <96300465+Chedeta@users.noreply.github.com>
2022-08-25 11:21:12 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import page1
+import page2
+import streamlit as st
+PAGES = {
+    "Dashboard": page1,
+    "Prédiction": page2,
+}
+st.sidebar.title('Navigation : ')
+selection = st.sidebar.selectbox("Aller :", PAGES.keys()))
+page = PAGES[selection]
+page.app()
