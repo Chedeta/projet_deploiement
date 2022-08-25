1d6ae6dbeb59443c68bc09a71a7affe912b5937b
Chedeta <96300465+Chedeta@users.noreply.github.com>
2022-08-25 11:46:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-import page1
-import page2
 import streamlit as st
-PAGES = {
-    "Dashboard": page1,
-    "Prédiction": page2,
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
 }
-st.sidebar.title('Navigation : ')
-selection = st.sidebar.selectbox("Aller :", PAGES.keys())
-page = PAGES[selection]
-page.app()
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
