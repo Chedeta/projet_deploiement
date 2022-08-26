783a443d4d05b3d6eaec7f5b9902bc28888bef0e
Chedeta <96300465+Chedeta@users.noreply.github.com>
2022-08-26 13:29:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def page2():
     df_delay_stat_treshold = pd.DataFrame({'Threshold (min)': x_plot.values(),'Rent_lost_mobile(%)': y_mobile_ratio.values(),'Rent_lost_connect(%)': y_connect_ratio.values()})
 
     st.line_chart(data=df_delay_stat_treshold, x='Threshold (min)', y=["Rent_lost_mobile(%)", 'Rent_lost_connect(%)'], use_container_width=True)
-    data_load_state = st.text('')
+    data_load_state = st.text(' ')
     delay = st.slider('Quel délai en deux locations (en minutes) :', 0, 400, 60)
     delay=int(delay)
     st.write(f'Pourcentage de location perdue sur **mobile** pour un délai de {delay} minutes : **{(df_delay_stat_treshold.iloc[delay][1])/100:.2%}**')
