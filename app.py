783ae5b15fe4daca378956a2c5f1948d446da1ec
Chedeta <96300465+Chedeta@users.noreply.github.com>
2022-11-05 14:22:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,19 @@ def main_page():
         it. Our data comes from an e-commerce website that simply displays samples of customer sales. Let's check it out.
         Also, if you want to have a real quick overview of what streamlit is all about, feel free to watch the below video 👇
     """)
+    dataset_delay = load_data2()
+    rental_delay = dataset_delay[dataset_delay['previous_rental_checkout_delay_in_minutes'] > 0]
     st.header('Main metrics of dataset')
     main_metrics_cols = st.columns([20,30,50])
-    nb_rentals = len(data2)
+    nb_rentals = len(dataset_delay)
     with main_metrics_cols[0]:
         st.metric(label = "Number of rentals", value= nb_rentals)
-        st.metric(label = "Number of cars", value= data2['car_id'].nunique())
+        st.metric(label = "Number of cars", value= dataset_delay['car_id'].nunique())
     with main_metrics_cols[2]:
-        st.metric(label = "Share of 'Connect' rentals", value= f"{round(len(data2[data2['checkin_type'] == 'connect']) /nb_rentals * 100)}%")
+        st.metric(label = "Share of 'Connect' rentals", value= f"{round(len(dataset_delay[dataset_delay['checkin_type'] == 'connect']) /nb_rentals * 100)}%")
     with main_metrics_cols[1]:
-        st.metric(label = "Share of consecutive rentals of a same car", value= f"{round(len(data2[~data2['previous_ended_rental_id'].isna()]) /nb_rentals * 100)}%")
+        st.metric(label = "Share of consecutive rentals of a same car", value= f"{round(len(dataset_delay[~dataset_delay['previous_ended_rental_id'].isna()]) /nb_rentals * 100)}%")
+         st.metric(label = "Max. delta between consecutive rentals", value= f"{round(rental_delay['time_delta_with_previous_rental_in_minutes'].max())} minutes")
     st.markdown("---")
 
 def page2():
